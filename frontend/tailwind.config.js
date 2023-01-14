ace62dc7b9ae7aeb1c68463fca3b0e2f135f73d7
TC Wang <boyw165@gmail.com>
2023-01-13 21:35:07 -0800

    Frontend: add TailwindCSS and copy some CSS boilerplate.

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/**/*.html',
+    "./pages/**/*.{html,js,jsx,ts,tsx}",
+    "./components/**/*.{html,js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
